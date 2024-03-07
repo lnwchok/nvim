031862fcc4d14d01f21e215b9afabdffec07d3e3
lnwchok <sitthichok.punlabpho@gmail.com>
2024-03-07 22:09:35 +0700

    Add Mason

---
+++ lua/core/plugin_config/lsp_config.lua
@@ -0,0 +1,24 @@
+require("mason").setup()
+require("mason-lspconfig").setup {
+  ensure_installed = { "lua_ls" },
+}
+
+local on_attach = function(_,bufr)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+end
+
+
+require("lspconfig").lua_ls.setup {
+  on_attach = on_attach
+}
+
+-- After setting up mason-lspconfig you may set up servers via lspconfig
+-- require("lspconfig").lua_ls.setup {}
+-- require("lspconfig").rust_analyzer.setup {}
+-- ...
